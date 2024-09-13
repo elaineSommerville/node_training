7df2a1593e45674348cf17af71a939e6e9effb01
Elaine Sommerville <s3058560@ecs166728.local>
2024-09-13 15:37:59 +0100

    ch04_02 reading ipsum file done

--- Exercise Files/Ch04/04_02/start/list.js
+++ Exercise Files/Ch04/04_02/start/list.js
@@ -2,6 +2,13 @@
 const fs = require("fs");
 
 //read the readme file and use the UTF-8 encoding 
-let ipsum = fs.readFileSync("./readme.md", "utf-8");
+//let ipsum = fs.readFileSync("./readme.md", "utf-8");
 
-console.log(ipsum);
+//another way to do what we've just done on line 5
+//non-blocking version
+//this time we also pass in an error
+fs.readFile("./readme.md", "utf-8", (err, ipsum) => {
+  console.log(ipsum);
+});
+
+console.log("reading the file ...");
